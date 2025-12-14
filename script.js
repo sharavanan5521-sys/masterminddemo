4a43cec35e647fbb7fb78f6c8ca48934831124f8
SHARAVANAN <sharavanan5521@gmail.com>
2025-12-14 21:19:10 +0800

    fixed responsive issue

--- script.js
+++ script.js
@@ -27,5 +27,31 @@ counters.forEach(counter => {
     updateCounter();
 });
 
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click', () => {
+        // Toggle Nav
+        nav.classList.toggle('nav-active-mobile');
+
+        // Animate Links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+            }
+        });
+
+        // Burger Animation
+        burger.classList.toggle('toggle');
+    });
+}
+
+// Invoke the function
+navSlide();
+
 // Simple Console log to show developer activity
 console.log("Nexus Digital: System Operational");
